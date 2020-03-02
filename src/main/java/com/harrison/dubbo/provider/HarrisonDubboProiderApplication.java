b85476c4e0bd252605f24ade6fbf574defb42929
harrison <a847381241@163.com>
2020-03-02 20:54:04 +0800

    Initial commit

---
+++ src/main/java/com/harrison/dubbo/provider/HarrisonDubboProiderApplication.java
@@ -0,0 +1,13 @@
+package com.harrison.dubbo.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarrisonDubboProiderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarrisonDubboProiderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/harrison/dubbo/provider/HarrisonDubboProiderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harrison.dubbo.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarrisonDubboProiderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
